23e2eab0bde9302281beb6807d965bd517860829
ericeberg3 <139914913+ericeberg3@users.noreply.github.com>
2024-09-03 15:56:22 -0700

    Fixed some plotting issues with the vector plot. Now the tilt greens functions make sense on the plot. Also changed the end time slightly and a few other minor tweaks.

--- createtiltgreens.m
+++ createtiltgreens.m
@@ -20,6 +20,7 @@ function [gTiltHMM, gTiltSC] = createtiltgreens(mHMM, mSC, dtheta, keep)
     gTiltHMM = -real([atan2(dHMM(3), 1), atan2(dHMM(6), 1)]) .* 1e6; % real([atan2(dHMM(3) * cos(dtheta) - dHMM(6) * sin(dtheta), 1), atan2(dHMM(3) * sin(dtheta) + dHMM(6) * cos(dtheta), 1)]) .* 1e6;
     gTiltSC = -real([atan2(dSC(3), 1), atan2(dSC(6), 1)]) .* 1e6; % real([atan2(dSC(3) * cos(dtheta) - dSC(6) * sin(dtheta), 1), atan2(dSC(3) * sin(dtheta) + dSC(6) * cos(dtheta), 1)]) .* 1e6;
     
+    
     if(keep)
         save Data/gTiltHMM.mat
         save Data/gTiltSC.mat
